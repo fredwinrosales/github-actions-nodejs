d303c9b31b00e79a5c02db6488f1504bc88eff25
Fredwin Rosales <fredwinrosales@gmail.com>
2025-03-21 22:26:38 +0000

    Crear API

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // index.js
-console.log("Hello World from Node.js");
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World from Node.js');
+});
+
+app.listen(port, () => {
+  console.log(`🚀 API escuchando en http://localhost:${port}`);
+});
