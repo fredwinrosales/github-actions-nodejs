3465dd37fad7b4baee921683f6127a5d8b7a1ff6
Fredwin Rosales <fredwinrosales@gmail.com>
2025-03-21 23:32:03 +0000

    Retornar un mensaje json

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Node.js');
+  res.send({'message':'Hello World from Node.js'});
 });
 
 app.listen(port, () => {
